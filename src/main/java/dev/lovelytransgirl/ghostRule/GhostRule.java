fe218a55cc62c6052b47ebd2d5786e2133f40806
lovelytransgirl <furries@duck.com>
2025-04-25 07:28:08 +0700

    CoreProtect API testing

--- src/main/java/dev/lovelytransgirl/ghostRule/GhostRule.java
+++ src/main/java/dev/lovelytransgirl/ghostRule/GhostRule.java
@@ -6,8 +6,11 @@ import io.papermc.paper.threadedregions.scheduler.AsyncScheduler;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
 
 import java.io.File;
 import java.util.logging.Level;
@@ -56,9 +59,33 @@ public final class GhostRule extends JavaPlugin {
         AsyncScheduler asyncScheduler = getServer().getAsyncScheduler();
         Start task = new Start(asyncScheduler);
         task.run(this);
+        CoreProtectAPI api = getCoreProtect();
+        if (api != null){ // Ensure we have access to the API
+            api.testAPI(); // Will print out "[CoreProtect] API test successful." in the console.
+        }
     }
+
     public static GhostRule getInstance() {
         return instance;
+    }
+
+    public CoreProtectAPI getCoreProtect() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+        if (plugin == null || !(plugin instanceof CoreProtect)) {
+            return null;
+        }
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (CoreProtect.isEnabled() == false) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 10) {
+            return null;
+        }
+
+        return CoreProtect;
     }
 
     @Override
